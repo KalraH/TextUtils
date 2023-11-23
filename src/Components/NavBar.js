05127e56dccd5206d1a2f916bdc330d58f75cb99
Hariom Kalra <kalrahariom2001@gmail.com>
2023-11-23 19:01:58 +0530

    Solved a bug where App was not showing anything when clicked on Home Button.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,7 +7,7 @@ export default function NavBar(props)
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
 			<div className="container-fluid">
-				<Link className="navbar-brand" to="/">{props.title}</Link>
+				<Link className="navbar-brand" to="/TextUtils">{props.title}</Link>
 				
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
@@ -16,7 +16,7 @@ export default function NavBar(props)
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 						<li className="nav-item">
-							<Link className="nav-link" aria-current="page" to="/">Home</Link>
+							<Link className="nav-link" aria-current="page" to="/TextUtils">Home</Link>
 						</li>
 						<li className="nav-item">
 							<Link className="nav-link" to="/about">{props.aboutText}</Link>
